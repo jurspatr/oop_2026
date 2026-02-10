a458643606257f2dbe9a9249fa6aae495383f493
Patrick Jurs <jurspatr@tlu.ee>
2026-02-10 16:55:42 +0000

    First homework: function about running

--- 01_funktsioon/homework1.js
+++ 01_funktsioon/homework1.js
@@ -1,28 +1,28 @@
-// Function 01
+// Homework 1
 // Arvuta jooksu tempo (min/km)
 function calculatePace(distanceKm, timeMinutes) {
-    // Step 01: Arvuta tempo kümnendvormis (minutit kilomeetri kohta)
+    //tempo kümnendvormis (minutit kilomeetri kohta)
     var totalPace = timeMinutes / distanceKm;
-    // Step 02: Eralda täisminutid
+    //eraldan täisminutid
     var minutes = Math.floor(totalPace);
-    // Step 03: Arvuta sekundid jääkosast (murdarv * 60)
+    //sekundid jääkosast (murdarv * 60)
     var seconds = Math.round((totalPace - minutes) * 60);
-    // Step 04: Vorminda sekundid tekstiks ja lisa vajadusel ette null
+    //vormindan sekundid tekstiks ja lisan vajadusel ette nulli
     var secondsText = seconds < 10 ? "0" + seconds : seconds;
-    // Return: Tagasta tulemus loetaval kujul
+    //tulemus loetaval kujul
     return minutes + ":" + secondsText + " min/km";
 }
-// Üksik test: 5km jooks ajaga 25 minutit
-console.log("Üksik test (5km, 25min): " + calculatePace(5, 25));
-// Erinevate distantside massiiv (kilomeetrites), mida läbiti näiteks 60 minutiga
+//5km jooks ajaga 25 minutit
+console.log("Test (5km, 25min): " + calculatePace(5, 25));
+//distantsid(kilomeetrites), mida läbiti 60 minutiga
 var distances = [8, 10, 12, 13.5, 15];
-// Kasuta map-meetodit, et arvutada tempo iga distantsi kohta, kui aeg on fikseeritud 60 min peale
+//tempo iga distantsi kohta, kui aeg on fikseeritud 60 min peale
 var paceResults = distances.map(function (d) {
     return calculatePace(d, 60);
 });
 console.log("Tempod, kui joosta 60 minutit erinevatel distantsidel:");
 console.log(paceResults);
-// Koosta tabel erinevate aegadega 10km distantsi puhul
+//tabel erinevate aegadega 10km distantsi puhul
 var runningTable = [];
 for (var time = 40; time <= 60; time += 5) {
     runningTable.push({
